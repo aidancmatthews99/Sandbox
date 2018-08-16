138abb6f141efe14311b33f27c8f991853d4a44b
aidancmatthews99 <aidan.matthews@my.jcu.edu.au>
2018-08-16 20:31:10 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,10 @@
 """Aidan"""
+
+MIN_LENGTH = 6
+password = input("Password: ")
+
+while len(password) < MIN_LENGTH:
+    print("Error! Password too short!")
+    password = input("Password: ")
+
+print("*" * len(password))
